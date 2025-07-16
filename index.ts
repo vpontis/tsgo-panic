acc53e4ca96d4d1dedbbf868db50c53718477fe4
Victor Pontis <victor@pont.is>
2025-07-16 07:31:46 -0400

    Fix: Narrow return type

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
 import "knex";
 
-// Minimal UnionPick type
-type UnionPick<T, K extends keyof T> = T extends unknown ? Pick<T, K> : never;
-
 // Minimal reproduction of the module augmentation that causes tsgo panic
 declare module "knex" {
   namespace Knex {
     interface QueryInterface<TRecord extends {} = any> {
       firstOrFail<TKey extends keyof TRecord>(
         ...columns: TKey[]
-      ): Knex.QueryBuilder<TRecord, UnionPick<TRecord, TKey>> &
-        Promise<UnionPick<TRecord, TKey>>;
+      ): Knex.QueryBuilder<TRecord, Pick<TRecord, TKey>> &
+        Promise<Pick<TRecord, TKey>>;
     }
   }
 }
